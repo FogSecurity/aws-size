b78a8a9c7eb7f89ec060c7e2410fdc2646b6ad18
Jason Kao <jason@fogsecurity.io>
2025-07-24 15:27:44 -0400

    pull policy and parse document

--- aws-size.py
+++ aws-size.py
@@ -8,20 +8,38 @@ parser = argparse.ArgumentParser(prog='AWS Size')
 parser.add_argument("--profile")
 
 args = parser.parse_args()
-#list-policies
-
-#For reach policy, get size
-    #Get Policy 
-    #Get Policy Version
-
 session = boto3.Session(profile_name = args.profile)
 
 iam_client = session.client('iam')
 
 iam_policies_results = [
     iam_client.get_paginator('list_policies')
-    .paginate()
+    .paginate(Scope='Local'
+
+    )
     .build_full_result()
 ]
 
-print(iam_policies_results)
+customer_managed_policies = iam_policies_results[0]['Policies']
+
+for managed_policy in customer_managed_policies:
+    version = managed_policy['DefaultVersionId']
+    arn = managed_policy['Arn']
+    print(managed_policy['Arn'])
+
+    policy = iam_client.get_policy_version(
+        PolicyArn=arn,
+        VersionId=version
+    )
+
+    policy_doc = policy['PolicyVersion']['Document']
+
+    str_policy = json.dumps(policy_doc)
+    print(policy_doc)
+    print(type(str_policy))
+
+#list-policies
+
+#For reach policy, get size
+    #Get Policy 
+    #Get Policy Version

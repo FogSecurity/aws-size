fbebf0c07e7205d63746e5d698a08a33f38326f3
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2025-07-24 16:58:07 -0400

    Customer Managed Policy Scanning (#1)
    This is the initial PR for aws-size.
    
    Resource: AWS Customer Managed Policies
    
    This will output a list of Customer Managed IAM Policies and policies that are over 90% of the character size.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # aws-size
-Checks Hard to Find Size Limits and Usage for AWS
+Checks Hard to Find Size Limits and Usage for AWS.
+
+For support or questions, we can be reached at info@fogsecurity.io.
+
+## Overview
+
+AWS services and resources have limits that can impact development.  These limits (sometimes referred to as Service Quotas) can sometimes be adjustable (soft limits) or not (hard limits).  In some cases, these can make development difficult.  
+
+Currently, this tool focuses on AWS IAM and visibility into some of those limits that can impede development.  
+
+Imagine a scenario where someone is trying to apply least privilege and has appropriately used condition keys, granular IAM actions, and resources in their IAM policies.  They may need to add another statement or action, but if they're out of character space - they will need to think creatively on how to adjust the policy.  This tool will help bring visibility into those limits.
+
+## Running aws-size
+
+Prerequisites:
+* Python
+* AWS Credentials
+
+To install required libraries, `pip3 install -r requirements.txt` can be run.
+
+To run aws-size, the following command can be run:
+
+```
+python3 aws-size.py --profile <your_profile_here>
+```
+
+This will output a list of customer managed policies with usage limit over 90%.
+
+Example output:
+
+```
+Customer Managed Policies Scanned: 82
+Customer Managed Policies with usage over 90%: 2
+
+List of policies with more than 90% character usage: 
+arn:aws:iam::123412341234:policy/<bigpolicy>
+Policy Usage: 90.48%
+Characters Left: 585
+```
+
+## Coverage
+
+### IAM Managed Policies
+
+Current Limit: 6,144 characters.  
+
+Note: white space doesn't count.
+
+aws-size will check all customer managed policies.
+
+
+
+Check AWS Documentation for all of the IAM and AWS STS Quotas (Limits) [here](https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html).
---
+++ aws-size.py
@@ -0,0 +1,72 @@
+import argparse
+import boto3
+import botocore
+import json
+
+parser = argparse.ArgumentParser(prog='AWS Size')
+
+parser.add_argument("--profile")
+
+args = parser.parse_args()
+session = boto3.Session(profile_name = args.profile)
+
+iam_client = session.client('iam')
+
+iam_policies_results = [
+    iam_client.get_paginator('list_policies')
+    .paginate(Scope='Local'
+    )
+    .build_full_result()
+]
+
+customer_managed_policies = iam_policies_results[0]['Policies']
+managed_policies_stats = []
+warning_policies = []
+
+for managed_policy in customer_managed_policies:
+    version = managed_policy['DefaultVersionId']
+    arn = managed_policy['Arn']
+    name = managed_policy['PolicyName']
+
+    policy = iam_client.get_policy_version(
+        PolicyArn=arn,
+        VersionId=version
+    )
+
+    policy_doc = policy['PolicyVersion']['Document']
+
+    str_policy = json.dumps(policy_doc, indent=None, separators=(',', ':'))
+
+    #Strip white space
+    stripped_str_policy = str_policy.replace(" ", "")
+    char_count = len(stripped_str_policy)
+
+    usage = round(char_count / 6144, 4)
+    char_left = 6144 - len(stripped_str_policy)
+
+    managed_policies_stats.append({
+        'arn': arn,
+        'name': name,
+        'usage': usage,
+        'charleft': char_left
+    })
+
+    if usage > 0.90:
+        warning_policies.append({
+            'arn': arn,
+            'name': name,
+            'usage': usage,
+            'charleft': char_left
+        })
+    
+
+#Output Section
+print("Customer Managed Policies Scanned: " + str(len(managed_policies_stats)))
+print("Customer Managed Policies with usage over 90%: " + str(len(warning_policies)))
+print('\n')
+print("List of policies with more than 90% character usage: ")
+
+for policy in warning_policies:
+    print(policy['arn'])
+    print(f"Policy Usage: {policy['usage']:.2%}")
+    print("Characters Left: " + str(policy['charleft']) + '\n')

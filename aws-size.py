37aa2f1db945ea227a171424e8e26fb764d818f7
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2025-07-28 21:10:41 -0400

    Resource Selection (#16)
    Add scaffolding for resource selection.
    
    Currently only has 1 resource supported, but has an interactive prompt via questionary. TODO: Update output to standardize output functions.

--- aws-size.py
+++ aws-size.py
@@ -1,8 +1,6 @@
 import argparse
 import boto3
-import botocore
-import json
-import sys
+import questionary
 
 parser = argparse.ArgumentParser(prog='IAM Size')
 
@@ -10,6 +8,15 @@ parser.add_argument("--profile")
 parser.add_argument("--threshold", help='Set threshold for reporting (between 0 and 1).  Default is 90%')
 
 args = parser.parse_args()
+
+supported_limits = [
+        "AWS IAM Managed Policies"
+]
+
+limit = questionary.select(
+    "Select a resource limit",
+    choices=supported_limits,
+).ask()
 
 try:
     if args.threshold:
@@ -27,68 +34,71 @@ except:
     print("Threshold must be a number between 0 and 1.  Running aws-size with default of 90%")
     threshold = 0.90
 
-try:
-    session = boto3.Session(profile_name = args.profile)
-    iam_client = session.client('iam')
-except:
-    print("Potential authentication issue: check credentials and try again")
-    sys.exit()
-
-try:
-    iam_policies_results = [
-        iam_client.get_paginator('list_policies')
-        .paginate(Scope='Local'
-        )
-        .build_full_result()
-    ]
-except:
-    print("Issue with listing IAM managed policies")
-    sys.exit()
-
-customer_managed_policies = iam_policies_results[0]['Policies']
-managed_policies_stats = []
-warning_policies = []
-
-for managed_policy in customer_managed_policies:
-    version = managed_policy['DefaultVersionId']
-    arn = managed_policy['Arn']
-    name = managed_policy['PolicyName']
+if limit == 'AWS IAM Managed Policies':
+    try:
+        session = boto3.Session(profile_name = args.profile)
+        iam_client = session.client('iam')
+    except:
+        print("Potential authentication issue: check credentials and try again")
+        sys.exit()
 
     try:
-        policy = iam_client.get_policy_version(
-            PolicyArn=arn,
-            VersionId=version
-        )
-
-        policy_doc = policy['PolicyVersion']['Document']
-
-        str_policy = json.dumps(policy_doc, indent=None, separators=(',', ':'))
-
-        #Strip white space
-        stripped_str_policy = str_policy.replace(" ", "")
-        char_count = len(stripped_str_policy)
-
-        usage = round(char_count / 6144, 4)
-        char_left = 6144 - len(stripped_str_policy)
-
-        if usage >= threshold:
-            warning_policies.append({
-                'arn': arn,
-                'name': name,
-                'usage': usage,
-                'charleft': char_left
-            })
-    
+        iam_policies_results = [
+            iam_client.get_paginator('list_policies')
+            .paginate(Scope='Local'
+            )
+            .build_full_result()
+        ]
     except:
-        print(f"Issue processing policy: {arn}")
+        print("Issue with listing IAM managed policies")
+        sys.exit()
 
-#Output Section
-print("Customer Managed Policies Scanned: " + str(len(managed_policies_stats)))
-print(f"Customer Managed Policies with usage over {threshold:.2%} " + str(len(warning_policies)))
-print('\n')
-print(f"List of policies with more than {threshold:.2%} character usage: ")
+    customer_managed_policies = iam_policies_results[0]['Policies']
+    managed_policies_stats = []
+    warning_policies = []
 
-for policy in warning_policies:
-    print(policy['arn'])
-    print(f"Policy Usage: {policy['usage']:.2%}")
-    print("Characters Left: " + str(policy['charleft']) + '\n')
+    for managed_policy in customer_managed_policies:
+        version = managed_policy['DefaultVersionId']
+        arn = managed_policy['Arn']
+        name = managed_policy['PolicyName']
+
+        try:
+            policy = iam_client.get_policy_version(
+                PolicyArn=arn,
+                VersionId=version
+            )
+
+            policy_doc = policy['PolicyVersion']['Document']
+
+            str_policy = json.dumps(policy_doc, indent=None, separators=(',', ':'))
+
+            #Strip white space
+            stripped_str_policy = str_policy.replace(" ", "")
+            char_count = len(stripped_str_policy)
+
+            usage = round(char_count / 6144, 4)
+            char_left = 6144 - len(stripped_str_policy)
+
+            if usage >= threshold:
+                warning_policies.append({
+                    'arn': arn,
+                    'name': name,
+                    'usage': usage,
+                    'charleft': char_left
+                })
+        
+        except:
+            print(f"Issue processing policy: {arn}")
+
+    #Eventually standardize output here
+    #Output Section
+    print("Customer Managed Policies Scanned: " + str(len(managed_policies_stats)))
+    print(f"Customer Managed Policies with usage over {threshold:.2%} " + str(len(warning_policies)))
+    print('\n')
+    print(f"List of policies with more than {threshold:.2%} character usage: ")
+
+    
+    for policy in warning_policies:
+        print(policy['arn'])
+        print(f"Policy Usage: {policy['usage']:.2%}")
+        print("Characters Left: " + str(policy['charleft']) + '\n')

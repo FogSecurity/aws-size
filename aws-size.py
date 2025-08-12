fb4ea20406fc6016394e59c290008b9425fa93f4
Jason Kao <jason@fogsecurity.io>
2025-08-12 10:59:16 -0400

    add role inline policies

--- aws-size.py
+++ aws-size.py
@@ -19,6 +19,9 @@ supported_limits = [
         "AWS IAM Managed Policies",
         "AWS IAM Role Trust Policy",
         "AWS IAM Managed Policies Per Role",
+        "AWS IAM Role Inline Policies",
+        "AWS IAM User Inline Policies",
+        "AWS IAM Group Inline Policies",
         "AWS EC2 User Data",
         "S3 Bucket Policy",
         "Organizations SCPs",
@@ -293,6 +296,95 @@ elif limit == "AWS IAM Managed Policies Per Role":
 
     save_output_to_file(warning_roles)
 
+elif limit == "AWS IAM Role Inline Policies":
+    try:
+        session = boto3.Session(profile_name = args.profile)
+        iam_client = session.client('iam')
+    except:
+        print("Potential authentication issue: check credentials and try again")
+        sys.exit()
+
+    try:
+        iam_roles_results = [
+            iam_client.get_paginator('list_roles')
+            .paginate()
+            .build_full_result()
+        ]
+    except:
+        print("Issue with listing IAM roles")
+        sys.exit()
+
+    roles = iam_roles_results[0]['Roles']
+    warning_roles = []
+
+    for role in roles:
+        arn = role['Arn']
+        name = role['RoleName']
+
+        role_inline_policy_size = 0
+
+        try:
+            role_inline_policies_results = [
+                iam_client.get_paginator('list_role_policies')
+                .paginate(RoleName=name)
+                .build_full_result()
+            ]
+
+            if role_inline_policies_results[0].get('PolicyNames'):
+                inline_policies = role_inline_policies_results[0]['PolicyNames']
+            else:
+                inline_policies = []
+        
+            for inline_policy in inline_policies:
+
+                policy = iam_client.get_role_policy(
+                    RoleName=name,
+                    PolicyName=inline_policy
+                )
+
+                policy_doc = policy['PolicyDocument']
+
+                str_policy = json.dumps(policy_doc, indent=None, separators=(',', ':'))
+
+                #Strip white space
+                stripped_str_policy = str_policy.replace(" ", "")
+                char_count = len(stripped_str_policy)
+
+                role_inline_policy_size += char_count
+
+            usage = round(role_inline_policy_size / 10240, 4)
+            char_left = 10240 - role_inline_policy_size
+
+            if usage >= threshold:
+                warning_roles.append({
+                    'arn': arn,
+                    'name': name,
+                    'usage': usage,
+                    'charleft': char_left
+                })        
+        
+        except:
+            print("Issue with listing inline policies for role: " + name)
+            continue
+
+    #Eventually standardize output here
+    #Output Section
+    print("IAM Roles Scanned: " + str(len(roles)))
+    print(f"IAM Roles with inline policy usage over {threshold:.2%} " + str(len(warning_roles)))
+    print('\n')
+
+    if len(warning_roles) > 0:
+        print(f"List of roles with more than {threshold:.2%} inline policy character usage: ")
+
+        for role in warning_roles:
+            print(role['arn'])
+            print(f"Role Name: {role['name']}")
+            print(f"Inline Policy Usage: {role['usage']:.2%}")
+            print("Characters Left: " + str(role['charleft']) + '\n')
+
+    save_output_to_file(warning_roles)
+
+    
 elif limit == "AWS EC2 User Data":
     try:
         session = boto3.Session(profile_name = args.profile, region_name = args.region)
